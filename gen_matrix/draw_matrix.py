31050e95e939b13356a863abcf556ee68d22d186
Christina Che Sun <che.sun.1996@gmail.com>
2023-11-22 23:27:51 -0800

    draw border, half and complete matrices

--- gen_matrix/draw_matrix.py
+++ gen_matrix/draw_matrix.py
@@ -4,91 +4,195 @@ import os
 from PIL import Image
 from random import randint
 
-# function to draw square matrix with red and black dots 
+
+# function to draw matrix with red and black dots - draws both half and the complete matrix
 # canvas_dim --> width and height (px) of a square canvas, make sure it is divisible by matrix_dim
 # matrix_dim --> dimension of dot matrix, equal to number of dots on one side
 # p --> probability of red for bernoulli trial of drawing each dot
-def draw(canvas_dim, matrix_dim, p, save_path, file_name_shell): 
-    # set the width and height of turtle window
-    window_w = canvas_dim + 20
-    window_h = canvas_dim + 20
+# top_half: whether draw top or bottom half, determined by the draw pair function
 
+
+def draw(canvas_dim, matrix_dim, p, save_path, file_name_shell, top_half): 
+    #############################################################
+    # canvas and screen setup
+    
+
+
+    # turtle initialization
     # initialize an instance of the turtle screen and pen 
     screen = turtle.Screen()
-    # turn off tracer
+    pen = turtle.Turtle()
+    pen.hideturtle()
     screen.tracer(0)
-    pen = turtle.Turtle() 
+    pen.speed(0) # the fastest turtle speed    
+    
     tsize = pen.turtlesize()
-    pen.speed(10)
+
+    # set the width and height of turtle window
+    window_w = canvas_dim + 50
+    window_h = canvas_dim + 50
+    # padding for the matrix, if matrix bigger than 10 x 10 use twice the padding
+    pad = canvas_dim/(2 * matrix_dim) if matrix_dim <= 10 else canvas_dim/matrix_dim
+    # space between dots
+    space = (canvas_dim - 2 * pad)/(matrix_dim - 1)
+    # starting coordinates for the full matrix
+    starting_x = - canvas_dim/2 + pad
+    starting_y = canvas_dim/2 - pad
 
     # set turtle window size
     screen.setup(window_w, window_h)
     # set turtle canvas size
     screen.screensize(canvas_dim, canvas_dim)
 
-    # print('screen size: ', screen.screensize(), 'window width: ', screen.window_width(), 'window height: ', screen.window_height())
+    # function to draw dots, starting from wherever the pen is currently
+    # dim_x: number of dots horizontally
+    # dim_y: number of dots vertically
+    def draw_dots(dim_x, dim_y, space, p):
+        
+        pen.setheading(0)
+        # initialize a var for number of red dots
+        n_red = 0
+        for i in range(dim_y): 
+            for j in range(dim_x): 
+                # each dot is a bernoulli trial with probability p
+                pen.pendown()
+                if randint(1, 100) <= p * 100:
+                    pen.color("black")
+                    pen.dot() 
+                else:
+                    pen.color("red")
+                    pen.dot()
+                    n_red += 1
+                pen.penup()
+                # distance for another dot 
+                pen.forward(space) 
+            pen.backward(space * dim_x) 
+            
+            # move turtle to start of next row
+            pen.right(90) 
+            pen.forward(space) 
+            pen.left(90) 
 
-    # padding for the matrix
-    pad = canvas_dim/(2 * matrix_dim)
-    # space between dots
-    space = (canvas_dim - 2 * pad)/(matrix_dim - 1)
+        # refresh the screen to show drawing
+        screen.update()
+        return n_red
 
-    # make pen go to starting position
+
+    # function for saving and converting files,  handles top/bottom half or whole matrix file name conversion
+    def save_graph():
+        # file path and name
+        if draw_half == True and draw_top == True:
+            file_name = file_name_shell.format(matrix_dim, red_total) + "_top_half"
+        elif draw_half == True and draw_top == False: 
+            file_name = file_name_shell.format(matrix_dim, red_total) + "_bot_half" 
+        elif draw_half == False:
+            file_name = file_name_shell.format(matrix_dim, red_total)
+        eps_complete_name = os.path.join(save_path, file_name+'.eps')
+        png_complete_name = os.path.join(save_path, file_name+'.png')
+        # grab the screen and export to eps file 
+        screen.getcanvas().postscript(file = eps_complete_name)
+        # convert eps to png
+        eps_image = Image.open(eps_complete_name)
+        eps_image.save(png_complete_name)
+
+        # delete eps file 
+        os.remove(eps_complete_name)
+
+ 
+    print("turtles before border: ", screen.turtles())
+    ##############################################################
+    # draw the border of the matrix
+    pen.color("black")
+    pen.setheading(0)
     pen.penup()
-    pen.goto(- canvas_dim/2 + pad, canvas_dim/2  - pad)
+    pen.goto(starting_x - pad, starting_y + pad) # top left corner
     pen.pendown()
 
-    # initialize a var for number of red dots
-    n_red = 0
-    for i in range(matrix_dim): 
-        for j in range(matrix_dim): 
-            
-            # each dot is a bernoulli trial with probability p
-            pen.pendown()
-            if randint(1, 100) <= p * 100:
-                pen.color("black")
-                pen.dot() 
-            else:
-                pen.color("red")
-                pen.dot()
-                n_red += 1
-            pen.penup()
-            # distance for another dot 
-            pen.forward(space) 
-        pen.backward(space * matrix_dim) 
-        
-        # move turtle to start of next row
-        pen.right(90) 
-        pen.forward(space) 
-        pen.left(90) 
-    
-    # print('number of red dots: ', n_red)
+    pen.goto(starting_x - pad, - starting_y - pad) # bottom left corner
+    pen.goto(- starting_x + pad, - starting_y - pad) # bottom right corner
+    pen.goto(- starting_x + pad, starting_y + pad) # top right corner
+    pen.goto(starting_x - pad, starting_y + pad) #  back to top left corner
 
-    # hide the turtle 
-    pen.hideturtle()
-    # refresh the screen to show drawing
+    pen.penup()
     screen.update()
-    # file path and name
-    file_name = file_name_shell.format(matrix_dim, n_red)
-    eps_complete_name = os.path.join(save_path, file_name+'.eps')
-    png_complete_name = os.path.join(save_path, file_name+'.png')
-    # grab the screen and export to eps file 
-    turtle.getscreen().getcanvas().postscript(file = eps_complete_name)
-    # print(eps_complete_name)
 
-    # convert eps to png
-    eps_image = Image.open(eps_complete_name)
-    eps_image.save(png_complete_name)
+    #########################################################
+    # first draw dots for half matrix
+    red_total = 0
+    matrix_height = int(matrix_dim/2)
+    # whether draw top or bottom half matrix, determine pen starting position for matrix
+    
+    
+    
+    # draw the top half
+    if top_half == True:
+        draw_half = True
+        draw_top = True
+        # determine starting point for the pen
+        pen.goto(starting_x, starting_y)
+        #################################################
+        # draw dots for the top half 
+        red_half = draw_dots(matrix_dim, matrix_height, space, p)
+        red_total=red_half
+        print("N red in top half: ", red_total)
+        save_graph()
 
-    # delete eps file 
-    os.remove(eps_complete_name)
-    # turtle.done()
-    # screen.mainloop()
+        # finish the complete matrix
+        draw_half = False
+        pen.goto(starting_x, starting_y - space * (matrix_dim / 2)) # starting position for drawing bottom half
+        red_total += draw_dots(matrix_dim, matrix_height, space, p)
+        print("N red in complete matrix: ", red_total)
 
-    return(n_red)
+        save_graph()
+
+    else: 
+        draw_half = True
+        draw_top = False
+        pen.goto(starting_x, starting_y - space * (matrix_dim / 2)) # starting position for drawing bottom half
+        #################################################
+        # draw dots for the bottom half 
+        red_half = draw_dots(matrix_dim, matrix_height, space, p)
+        red_total = red_half
+        print("N red in bottom half: ", red_total)
+
+        save_graph()
+
+        # finish the complete matrix
+        draw_half = False   
+        pen.goto(starting_x, starting_y)
+        red_total += draw_dots(matrix_dim, matrix_height, space, p)
+        print("N red in complete matrix: ", red_total)
+
+        save_graph()
+   
+    
+    detail_dict = {"half_position": "top" if draw_top == True else "bottom", "half red": red_half, "total red": red_total}
+    print(detail_dict)
+    print("turtles after drawing: ", screen.turtles())
+
+    # delete all drawings and turtles 
+    screen.clearscreen()
+
+    return detail_dict
 
 
 
+
+
+
+draw(canvas_dim= 400, matrix_dim= 10, p = 0.5, save_path='/Users/christinasun/Library/CloudStorage/Dropbox/github_repos/belief_distortion/gen_matrix/graphs/half', file_name_shell = 'dim_{}_r_{}', top_half= True)
+
+
+draw(canvas_dim= 400, matrix_dim= 10, p = 0.5, save_path='/Users/christinasun/Library/CloudStorage/Dropbox/github_repos/belief_distortion/gen_matrix/graphs/half', file_name_shell = 'dim_{}_r_{}', top_half= True)
+
+draw(canvas_dim= 400, matrix_dim= 20, p = 0.5, save_path='/Users/christinasun/Library/CloudStorage/Dropbox/github_repos/belief_distortion/gen_matrix/graphs/half', file_name_shell = 'dim_{}_r_{}', top_half=False)
+
+draw(canvas_dim= 400, matrix_dim= 20, p = 0.5, save_path='/Users/christinasun/Library/CloudStorage/Dropbox/github_repos/belief_distortion/gen_matrix/graphs/half', file_name_shell = 'dim_{}_r_{}', top_half=False)
+
+
+
+
+###################################################
 # a function to draw n pairs of matrices
 def draw_n_pairs(canvas_dim, matrix_dim, n, p):
     dir = '/Users/christinasun/Library/CloudStorage/Dropbox/github_repos/belief_distortion/gen_matrix/graphs'
@@ -109,12 +213,16 @@ def draw_n_pairs(canvas_dim, matrix_dim, n, p):
         os.remove(os.path.join(save_path, file.name))
 
 
+
+
     # a counter for the total number of times matrix pairs have the same number of red
     same_instance = 0
 
     nred_overall = []
     # draw matrix pairs
     for pairindex in range(n):
+        # coin flip for top or bottom half
+        top_half = True if randint(1, 100) <= 50 else False
         # a list for number of red in each matrix in this pair
         pair_nred = []
         # fill the matrix pair number in the prefix shell
@@ -122,20 +230,27 @@ def draw_n_pairs(canvas_dim, matrix_dim, n, p):
         print('completed file prefix: ', file_prefix)
         # draw 2 matrices
         for i in range(2):
-            nred = draw(canvas_dim = canvas_dim, matrix_dim= matrix_dim, p = p, save_path= save_path, file_name_shell= (file_prefix + file_name_shell))
+            # this is a dictionary for this particular drawing
+            nred = draw(canvas_dim = canvas_dim, matrix_dim= matrix_dim, p = p, save_path= save_path, file_name_shell= (file_prefix + file_name_shell), top_half= top_half)
+            nred["pair number"] = pairindex + 1
             pair_nred.append(nred)
         # check if the two matrices have the same number of red
-        while pair_nred[1] == pair_nred[0]:
+        while pair_nred[1]["total red"] == pair_nred[0]["total red"]:
             same_instance += 1
-            pair_nred[1] = draw(canvas_dim = canvas_dim, matrix_dim= matrix_dim, p = p, save_path= save_path, file_name_shell= (file_prefix + file_name_shell))
+            pair_nred[1] = draw(canvas_dim = canvas_dim, matrix_dim= matrix_dim, p = p, save_path= save_path, file_name_shell= (file_prefix + file_name_shell), top_half=top_half)
         nred_overall.append(pair_nred)
     print('This is the number of times matrix pairs had same n_red: ', same_instance)
-    print('number of red dots in all matrix pairs: ', nred_overall)
+    print('details on the matrix pairs: ', nred_overall)
     return nred_overall
 
+
+
+
 # call drawing program
-draw_n_pairs(canvas_dim= 200, matrix_dim= 10, n = 10, p = 0.5)
+# draw_n_pairs(canvas_dim= 300, matrix_dim= 10, n = 10, p = 0.5)
 
-draw_n_pairs(canvas_dim= 200, matrix_dim= 20, n = 10, p = 0.5)
+# draw_n_pairs(canvas_dim= 400, matrix_dim= 20, n = 10, p = 0.5)
 
+# draw(canvas_dim= 400, matrix_dim= 10, p = 0.5, save_path='/Users/christinasun/Library/CloudStorage/Dropbox/github_repos/belief_distortion/gen_matrix/graphs', file_name_shell = 'dim_{}_r_{}')
 
+# draw(canvas_dim= 400, matrix_dim= 10, p = 0.5, save_path='/Users/christinasun/Library/CloudStorage/Dropbox/github_repos/belief_distortion/gen_matrix/graphs', file_name_shell = 'dim_{}_r_{}')
